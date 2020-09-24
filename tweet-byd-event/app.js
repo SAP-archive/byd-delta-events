f7b9f46e438efe6e182c891e68a6533b3a68e0ae
Ralph Oliveira <ralphrpo@gmail.com>
2020-09-24 12:35:31 +0100

    implement twitter event handler

--- tweet-byd-event/app.js
+++ tweet-byd-event/app.js
@@ -1,10 +1,39 @@
-/**
- * A Lambda function that returns a static string
- */
-exports.lambdaHandler =  async (event, context) => {
-    // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = 'Lets get this tweeted';
-    console.info(`${message}`);
-    console.log(event.Records[0].Sns.Message)
-    return message;
+const config = {
+    consumer_key: process.env.TWITER_API_KEY,
+    consumer_secret: process.env.TWITTER_SECRET_KEY,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_TOKEN_SECRET
+}
+const twitter = require('twitter');
+var client = new twitter(config);
+
+exports.lambdaHandler = async (event, context) => {
+    try {
+        const toTweet = prepareTweet(event.Records[0].Sns.Message)
+        await client.post('statuses/update', {status: toTweet})
+        .then(function (tweet) {
+            console.log(tweet);
+        })
+        .catch(function (error) {
+            throw error;
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function prepareTweet(message) {
+    // Returns Configuration Record from DynamoDB
+    console.log("Extracting datat of SNS message " + message)
+    try {
+        var parserMessage = JSON.parse(message)
+        var tweet = `${parserMessage.GenericType} - ${parserMessage.GenericId} ` +
+            `was ${parserMessage.Updated?"updated":"created"} ` +
+            `on our system @ ${parserMessage.DateStr} #KeepTheCoreClean`
+        console.log(tweet)
+        return tweet
+
+    } catch (error) {
+        throw new Error(error)
+    }
 }
