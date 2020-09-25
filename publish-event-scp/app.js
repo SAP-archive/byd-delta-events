e1ff18e5c63d997b7be84f507a1819a98aba0448
Ralph Oliveira <ralphrpo@gmail.com>
2020-09-25 10:58:11 +0100

    Retrieve token from SCP Enterprise messaging

--- publish-event-scp/app.js
+++ publish-event-scp/app.js
@@ -1,9 +1,61 @@
 /**
  * A Lambda function that returns a static string
  */
+const axios = require("axios")
+
 exports.lambdaHandler = async (event, context) => {
-    const message = 'Lets send tthis to SCP';
-    console.info(`${message}`);
-    console.log(event.Records[0].Sns.Message)
+    const message = 'Lets send this to SCP';
+    
+
+    const token = await getEmToken()
+
     return message;
+
 }
+
+let getEmToken = function () {
+    return new Promise(function (resolve, reject) {
+
+        console.log("Preparing request to SCP Messaging")
+
+        var params = new URLSearchParams({
+            "grant_type": "client_credentials",
+            "response_type": "token",
+        })
+
+        const options = {
+            method: "POST",
+            baseURL: process.env.SCP_EM_URL,
+            url:  process.env.SCP_EM_AUTH_ENDPOINT,
+            headers: {
+                "Content-Type": " application/x-www-form-urlencoded",
+                "Authorization": "Basic " + process.env.SCP_EM_AUTHORIZATION
+
+            },
+            params: params
+        }
+
+        console.log("REQUEST!!!!!")
+        console.dir(options)
+
+        // console.debug(options)
+
+        axios.request(options).then((response) => {
+                console.log(`SCP Response: is ${response.status} - ${response.statusText}`)
+                if (response.statusCode < 200 || response.statusCode >= 300) {
+                    return reject(
+                        new Error(`${response.statusCode}: ${response.req.getHeader("host")} ${response.req.path}`)
+                    );
+                } else {
+                    console.log("Received token is ")
+                    console.log(response.data.access_token)
+                    return resolve(response.data.access_token)
+                }
+            })
+            .catch((err) => {
+                console.error("Error calling SCP to retrieve Token -" + err)
+                reject(new Error(err));
+            })
+    })
+}
+
