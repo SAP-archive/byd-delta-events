f9f89389f34847fd1e732419d94f6bf32b2cfa4b
Ralph Oliveira <ralphrpo@gmail.com>
2020-09-25 11:17:15 +0100

    publish to SCP

--- publish-event-scp/app.js
+++ publish-event-scp/app.js
@@ -5,9 +5,9 @@ const axios = require("axios")
 
 exports.lambdaHandler = async (event, context) => {
     const message = 'Lets send this to SCP';
-    
 
     const token = await getEmToken()
+    const msgPublished = await publishMsgSCP(token)
 
     return message;
 
@@ -54,6 +54,45 @@ let getEmToken = function () {
             })
             .catch((err) => {
                 console.error("Error calling SCP to retrieve Token -" + err)
+                reject(new Error(err));
+            })
+    })
+}
+
+
+let publishMsgSCP = function (token) {
+    return new Promise(function (resolve, reject) {
+        console.log("Preparing request to SCP Messaging")
+
+        const options = {
+            method: "POST",
+            baseURL: process.env.SCP_EM_PUB_URL,
+            url:  process.env.SCP_EM_PUB_ENDPOINT,
+            headers: {
+                "x-qos": 0,
+                "Content-Type": "text/plain",
+                "Authorization": "Bearer " + token
+            },
+            data: {message:"Alo alo SCP. Aqui quem fala é a Lambda"}
+        }
+
+        console.log("REQUEST TO PUBLISH Message")
+        console.log(options)
+
+        // console.debug(options)
+        axios.request(options).then((response) => {
+                console.log(`SCP Response: is ${response.status} - ${response.statusText}`)
+                if (response.statusCode < 200 || response.statusCode >= 300) {
+                    return reject(
+                        new Error(`${response.statusCode}: ${response.req.getHeader("host")} ${response.req.path}`)
+                    );
+                } else {
+                    console.log("Message Published o SCP")
+                    return resolve()
+                }
+            })
+            .catch((err) => {
+                console.error("Error calling SCP to publish message -" + err)
                 reject(new Error(err));
             })
     })
